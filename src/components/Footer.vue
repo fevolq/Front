9761ee1416ee4eda8b4c0a5974b6f6705924a3e8
FuQiang <fuq15394@gmail.com>
2023-05-23 13:52:41 +0800

    footer bug待修复

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -28,17 +28,18 @@ async function getWitticism() {
 function getFonrSize(el) {
   const computedStyle = window.getComputedStyle(el)
   const fontSize = computedStyle.getPropertyValue('font-size')
-  return parseInt(fontSize.replace('px', ''))
+  const size = parseInt(fontSize.replace('px', ''))
+  return isNaN(size) ? 1 : size
 }
 
 async function scroll(containerEl, scrollEl, speed=5) {
   await getWitticism()
-  
   // 计算文本的宽度
   const textWidth = scrollEl.textContent.length * getFonrSize(scrollEl)
   
   // 设置动画的持续时间
   const animationDuration = textWidth / speed
+  // console.log(scrollEl.textContent.length, getFonrSize(scrollEl), animationDuration);
   
   const animation = scrollEl.animate(
     [
@@ -46,7 +47,7 @@ async function scroll(containerEl, scrollEl, speed=5) {
       { transform: `translateX(-${textWidth + 50}px)`},
     ],
     {
-      duration: animationDuration > 0 ? animationDuration * 500 : "auto",
+      duration: animationDuration > 0 ? parseInt(animationDuration) * 500 : "auto",
       iterations: 1,
       easing: 'linear',
     }
@@ -54,16 +55,21 @@ async function scroll(containerEl, scrollEl, speed=5) {
   animation.play()
 
   // 鼠标事件
-  containerEl.addEventListener('mouseenter', () => animation.pause())
-  containerEl.addEventListener('mouseleave', () => animation.play())
+  // containerEl.addEventListener('mouseenter', () => animation.pause())
+  // containerEl.addEventListener('mouseleave', () => animation.play())
 
-  animation.onfinish = () => scroll(containerEl, scrollEl, speed)
+  animation.onfinish = () => {
+    // containerEl.removeEventListener('mouseenter', () => animation.pause())
+    // containerEl.removeEventListener('mouseleave', () => animation.play())
+    scroll(containerEl, scrollEl, speed)
+    animation.finish()
+  }
 }
 
 onMounted(() => {
   const containerEl = document.querySelector('.scroll-container')
   const textEl = document.querySelector('.scroll-text')
-  scroll(containerEl, textEl, 5)
+  // scroll(containerEl, textEl, 5)
 })
 
 </script>
