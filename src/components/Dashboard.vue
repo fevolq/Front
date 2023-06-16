8aeca4cf5c6b7b33464f52f656af6ef404449d24
FuQiang <fuq15394@gmail.com>
2023-06-16 15:26:57 +0800

    过滤器插件

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,26 +2,39 @@
   <div class="container">
     <!-- 过滤器 -->
     <div class="filters">
-      <template v-for="filter in filters" :key="filter.name">
-        <template v-if="filter.type=='daterange'">
-          <dateRangeFilter :filterName="filter.name"/>
+      <div class="filter-area">
+        <template v-for="filter in filters" :key="filter.name">
+          <template v-if="filter.type=='daterange'">
+            <dateRangeFilter :filterName="filter.name"/>
+          </template>
+          <template v-else-if="filter.type=='input'">
+            <inputFilter :filterName="filter.name"/>
+          </template>
+          <template v-else-if="filter.type=='inputs'">
+            <inputsFilter :filterName="filter.name"/>
+          </template>
+          <template v-else-if="filter.type=='single' || filter.type=='multiple'">
+            <selectFilter :filterName="filter.name"/>
+          </template>
+          <template v-else>
+            {{ filter.title }} - {{ filter.type }}
+          </template>
+  
+          <!-- 过滤器插件 -->
+          <div class="plugin">
+            <el-tooltip effect="dark" :content="filter.exclude ? '排除' : '不排除'" placement="right">
+              <span v-if="filter.config.enableExclude" @click="filter.exclude = !filter.exclude" :class="filter.exclude ? 'exclude' : 'inexclude'">!</span>
+            </el-tooltip>
+            <el-tooltip effect="dark" :content="filter.expand ? '展开' : '不展开'" placement="right">
+              <span v-if="filter.config.enableExpand" @click="filter.expand = !filter.expand" class="expand"> {{ filter.expand ? '+' : '-' }}</span>
+            </el-tooltip>
+          </div>
         </template>
-        <template v-else-if="filter.type=='input'">
-          <inputFilter :filterName="filter.name"/>
-        </template>
-        <template v-else-if="filter.type=='inputs'">
-          <inputsFilter :filterName="filter.name"/>
-        </template>
-        <template v-else-if="filter.type=='single' || filter.type=='multiple'">
-          <selectFilter :filterName="filter.name"/>
-        </template>
-        <template v-else>
-          {{ filter.title }} - {{ filter.type }}
-        </template>
-      </template>
+      </div>
 
       <el-button class="search" :icon="doSearching ? Loading : Search" @click="handleSearch" circle :disabled="doSearching"/>
     </div>
+
 
     <!-- 数据 -->
     <div class="data">
@@ -34,7 +47,7 @@
 
 <script setup>
 import { ref, reactive, onBeforeMount, provide } from 'vue'
-import { Search, Loading } from '@element-plus/icons-vue'
+import { Search, Loading  } from '@element-plus/icons-vue'
 
 import dateRangeFilter from './filters/dateRangeFilter.vue'
 import inputFilter from './filters/inputFilter.vue'
@@ -70,15 +83,17 @@ class Filter {
     required: null,
   };
 
-  values = ref();
-  sendConfig = {};
-  expand = false;
+  values = ref(null);
+  expand = ref(false);
+  exclude = ref(false)
 
   constructor(config) {
     this.name = config['name']
     this.title = config['title']
     this.type = config['type']
     this.setConfig(config)
+
+    this.values.value = this.config.defaultValue
   }
 
   setConfig = (data) => {
@@ -99,7 +114,8 @@ class Filter {
     return {
       name: this.name,
       values: this.values.value,
-      // is_expand: this.expand,
+      is_expand: this.expand.value,
+      is_exclude: this.exclude.value,
     }
   }
 }
@@ -110,8 +126,8 @@ const filters = reactive({})
 const columns = ref([])
 const tableData = ref([])
 
-const getFilters = () => {
-  dashboardApi.getConfig(props.dashboardName)
+async function getFilters() {
+  await dashboardApi.getConfig(props.dashboardName)
   .then(res => {
     res['data']['filters'].forEach(data => {
       const oneFilter = new Filter(data)
@@ -123,11 +139,6 @@ provide('filters', {
   filters
 })
 
-onBeforeMount(() => {
-  getFilters()
-  getChart()
-})
-
 const getChart = () => {
   doSearching.value = true
   
@@ -135,7 +146,6 @@ const getChart = () => {
   for (let filterName in filters) {
     filtersValues.push(filters[filterName].getSendConfig())
   }
-  console.log(filtersValues);
   const query = {
     filters: filtersValues
   }
@@ -147,12 +157,21 @@ const getChart = () => {
     const resData = res['data']
     columns.value = resData['chart']['cols']
     tableData.value = resData['chart']['data']
-  })
+  }).catch(() => doSearching.value = false)
 }
 
 const handleSearch = () => {
   getChart()
 }
+
+async function init() {
+  await getFilters()
+  getChart()
+}
+
+onBeforeMount(() => {
+  init()
+})
 
 </script>
 
@@ -168,13 +187,14 @@ const handleSearch = () => {
   padding-bottom: 10px;
 }
 
-.filters .filter {
-  margin: 10px 20px;
+.filter-area {
+  display: flex;
+  margin-right: 30px;
 }
 
-.filters .required > span::after {
-  content: ' *';
-  color: red;
+.filter-area .filter {
+  margin: 10px 40px;
+  margin-right: 0;
 }
 
 .filter > span {
@@ -182,7 +202,29 @@ const handleSearch = () => {
   margin-right: 5px;
 }
 
-.filters > .search {
+.filter-area .required > span::after {
+  content: ' *';
+  color: red;
+}
+
+.filter-area .plugin {
+  margin: auto;
+  margin-left: 5px;
+  width: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.plugin > span {
+  cursor: pointer;
+}
+
+.plugin > .exclude {
+  color: red;
+}
+
+.filters .search {
   color: red;
   background-color: white;
 }
